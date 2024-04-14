ca7b75827099ab5350ce356faf6d8a044d3f56b7
ArthurRL <111669311+Arthu-RL@users.noreply.github.com>
2024-04-14 16:46:09 -0300

    Update Yolov7Pose-Detection.py

--- Yolov7Pose-Detection.py
+++ Yolov7Pose-Detection.py
@@ -11,6 +11,7 @@ from torchvision import transforms
 from utils.datasets import letterbox
 from utils.general import non_max_suppression_kpt
 from utils.plots import output_to_keypoint, plot_skeleton_kpts
+from utils.draw_kpts import draw_keypoints
 
 import matplotlib.pyplot as plt
 
@@ -87,26 +88,7 @@ while True:
 
     frame_count += 1
 
-    output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
-    output = output_to_keypoint(output)
-    nimg = image[0].permute(1, 2, 0) * 255
-    nimg = nimg.cpu().numpy().astype(np.uint8)
-    nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
-    for idx in range(output.shape[0]):
-        plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
-
-        # Comment/Uncomment the following lines to show bounding boxes around persons.
-        xmin, ymin = (output[idx, 2]-output[idx, 4]/2), (output[idx, 3]-output[idx, 5]/2)
-        xmax, ymax = (output[idx, 2]+output[idx, 4]/2), (output[idx, 3]+output[idx, 5]/2)
-        cv2.rectangle(
-            nimg, 
-            (int(xmin), int(ymin)),
-            (int(xmax), int(ymax)),
-            color=(255, 0, 0),
-            thickness=2, 
-            lineType=cv2.LINE_AA
-        )
-
+    nimg = draw_keypoints(model, out, image)
 
     cv2.putText(nimg, f"{fps:.1f} FPS", (15, 30), cv2.FONT_HERSHEY_SIMPLEX,
                 1, (0, 255, 0), 2)
